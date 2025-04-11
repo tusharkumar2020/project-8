9a2e18fcac891e44a364dd4aec0cfdac877bb1db
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-11 08:02:02 -0400

    completed task 1, 2, 3

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+def multiply(a, b):
+    result = a * b
+    return re
